eaa2a71110c0d38261c1058a40557befa89a7ae9
Dhanraj30 <dhanraj30102002@gmail.com>
2024-11-22 16:48:33 +0530

    change

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
-
+import dotenv from "dotenv";
+dotenv.config();
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
     NEXT_PUBLIC_MORALIS_API_KEY: process.env.NEXT_PUBLIC_MORALIS_API_KEY,
   },
-  // Additional config options
-  reactStrictMode: true,
+  
 };
 
 export default nextConfig;
