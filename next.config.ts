0f3a1832ce3f01ae41b3b11fe53feb676e521a56
Dhanraj30 <dhanraj30102002@gmail.com>
2024-11-22 16:35:00 +0530

    solve issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_MORALIS_API_KEY: process.env.NEXT_PUBLIC_MORALIS_API_KEY,
+  },
+  // Additional config options
+  reactStrictMode: true,
 };
 
 export default nextConfig;
