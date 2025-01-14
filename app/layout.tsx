1c70303ed23d38811eeb3b60a49e929b9eb94114
Dhanraj30 <dhanraj30102002@gmail.com>
2025-01-14 23:02:22 +0530

    Add: add dashboard page and modify changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { Providers } from './providers'
 import  {ThemeProvider} from '../components/theme-provider'
 import "./globals.css";
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
+    <html lang="en" suppressHydrationWarning className="dark">
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
